0b8525cad7b5a9a61db1e3f744c16729730af8b7
SUHAAS SWAMY MH <suhaasswamy1999@gmail.com>
2020-10-15 11:26:52 +0530

    merging branch

--- project/vieww.php
+++ project/vieww.php
@@ -45,79 +45,7 @@
 
         }
 
-        /* form {
-	background-color: #FFFFFF;
-	display: top;
-	align-items: center;
-	justify-content: center;
-	flex-direction: column;
-	padding: 20px 50px;
-	height: 150%;
-    text-align: center; */
-        /* overflow:scroll; */
-        /* position:fixed; */
-        /* margin-left:500px;
-    margin-top:70px;
-	
-} */
-
-        /* input {
-	background-color: lightgray;
-	border: none;
-	padding: 12px 15px;
-	margin: 8px 0;
-    width: 100%;
-    border-top-left-radius: 9px;
-    border-top-right-radius: 9px ;
-    border-bottom-left-radius: 9px;
-    border-bottom-right-radius: 9px;
-} */
-
-        /* .container {
-	background-color: #fff;
-	border-radius: 10px;
-  	box-shadow: 0 14px 28px rgba(0,0,0,0.25), 
-			0 10px 10px rgba(0,0,0,0.22);
-	position: relative;
-	overflow: hidden;
-	width: 768px;
-	max-width: 100%;
-    min-height: 480px;
-    overflow:scroll;
-    margin-left:200px;
-} */
-
-
-        /* .form-container { 
-	position: fixed;
-	top: 0;
-	height: 100%;
-	transition: all 0.6s ease-in-out;
-    overflow:hidden;   
-    align-content: center;                /*    overflow added here     */
-
-        /* } */
-
-        /* .sign-in-container {
-	left: 0;
-	width: 50%;
-	z-index: 2;
-	 */
-        /* } */
-
-        /* .container.right-panel-active .sign-in-container {
-	transform: translateX(100%);
-} */
-
-        /* .sign-up-container {
-	left: 0;
-	width: 35%;
-	opacity: 0;   
-	background-color:whitesmoke;
-    z-index: 1;	 
-    align-items: center;
-    /* overflow:sccroll; */
-        /* }  */
+   
 
         h3 {
             width: 50%;
@@ -132,44 +60,7 @@
 
 
 
-        /* 
-        ul {
-            list-style-type: none;
-            margin: 0;
-            padding: 0;
-            overflow: hidden;
-            background-color: black;
-        }
-
-        input {
-            color: white;
-            width: 10%;
-            height: 5%;
-        }
-
-        h1 {
-            background-color: #6e7b8b;
-            color: black;
-            text-align: center;
-        }
-
-        h2 {
-            color: #cccccc;
-        }
-
-        li {
-            display: inline;
-            float: left;
-        }
-
-        li a {
-            display: block;
-            color: #cccccc;
-            text-align: center;
-            padding: 16px 35px;
-            text-decoration: none;
-        } */
-
+      
         select {
             width: 20%;
             padding: 5px 15px;
